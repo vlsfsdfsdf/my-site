16bb700ec725c5ef9927542fdb598066b2e23532
vlsfsdfsdf <vlads3619@gmail.com>
2025-10-08 00:10:39 +0200

    I end 17-th lesson

--- modal.js
+++ modal.js
@@ -1,10 +1,8 @@
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector ('.modal-btn-open');
-const modalBtnClose = document.querySelector ('.modal-btn-close');
+const modal = document.querySelector(".backdrop");
+const modalBtnOpen = document.querySelector(".modal__btn-open");
+const modalBtnClose = document.querySelector(".modal__btn-close");
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+const toggleModal = () => modal.classList.toggle("is-hidden");
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
-
-
+modalBtnOpen.addEventListener("click", toggleModal);
+modalBtnClose.addEventListener("click", toggleModal);
